fd1db0b4a55696c49f9b61067d99b81d8e0d3df0
Rajkamal Singh <rajkamalsinghrn@gmail.com>
2024-09-11 17:57:24 +0530

    firsst commit

--- app/screens/afterLogin/Settings/Settings.tsx
+++ app/screens/afterLogin/Settings/Settings.tsx
@@ -1,20 +1,5 @@
 import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
-import Realm from 'realm';
-import RNFS from 'react-native-fs';
-
-// Define your schema
-const FileSchema = {
-  name: 'File',
-  properties: {
-    name: 'string',
-    data: 'data',
-  },
-};
-// Open Realm
-async function openRealm() {
-  return await Realm.open({schema: [FileSchema]});
-}
 
 const Settings = () => {
   return (
